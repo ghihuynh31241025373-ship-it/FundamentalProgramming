c5264c58e7c758c12b209fbfcf6092e596b77f8a
Asus <Asus@LAPTOP-043F70GA>
2026-05-22 16:25:38 +0700

    Hãy đưa tất cả lên github

---
+++ EX chap 4/EX chap 4/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Exercises
+{
+    static int MaxThreeNumbers(int a, int b, int c)
+    {
+        return Math.Max(a, Math.Max(b, c));
+    }
+    static int MaxNumbers(params int[]a)
+    {
+        int max = a[0];
+        foreach (int x in a)
+            if (x > max)
+                max = x;
+        return max; 
+
+    }
+    public static void Main(string[] args)
+    {
+        int max3 = MaxThreeNumbers(5, 9, 3);
+        Console.WriteLine("Max of 3 numbers: " + max3);
+
+        int m1 = MaxNumbers(1, 2, 3);
+        int m2 = MaxNumbers(1, 4, 8, 5);
+        int m3 = MaxNumbers(100);
+
+        Console.WriteLine("Max (1,2,3): " + m1);
+        Console.WriteLine("Max (1,4,5,): " + m2);
+        Console.WriteLine("Max (100): " + m3);
+    }
+}
---
+++ Ex-4/Ex-4/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    static int cong(int x, int y)
+    {  return x + y;
+    }
+    private static void Main(string[] args)
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        int c = cong(a, b);
+        Console.WriteLine($"{a} + {b} + {c}");
+    }
+}
